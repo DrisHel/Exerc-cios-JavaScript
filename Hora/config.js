8bf279593e516ea2599800406b7b1c22796eb72e
DrisHel <doris90nunes@gmail.com>
2021-11-02 23:17:50 -0300

    Estilo,configuraçao na pagina.

--- Hora/config.js
+++ Hora/config.js
@@ -1,1 +1,20 @@
-alert('Seja bem vindo!')
+function carregar(){
+
+    const mensagem = document.getElementById(mensagem);
+    const imagem  = document.getElementById(imagem);
+    const data = new Date;
+    const hora = date.getHours();
+    
+    mensagem.innerHTML = `Agora são ${hora} horas.`;
+
+    if(hora >=0 && hora <12){
+
+        imagem.src = manha.png;
+    }else if(hora >=12 && hora < 18){
+        imagem.src =tarde.png;
+    }else{
+        imagem.src =noite.png;
+    }
+};
+
+
